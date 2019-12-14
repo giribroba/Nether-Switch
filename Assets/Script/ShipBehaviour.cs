69442a8e8b54b1209733fa9cff6a2c0512c82bcb
giribroba <giribroba@gmail.com>
2019-12-14 15:40:42 -0300

    gameplay certa

--- Assets/Script/ShipBehaviour.cs
+++ Assets/Script/ShipBehaviour.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class ShipBehaviour : MonoBehaviour
 {
     [SerializeField]private float velocidade, raioRC, distCentro;
+    public int index;
     private Animator _animator;
     private Rigidbody2D rbPlayer;
-    private bool anguloDireita, executar, colidiu, anguloRaycast;
+    private bool anguloDireita, executar, anguloRaycast, colidiu;
+    Collider2D[] colididos;
     private string botaoPrincipal;
     [SerializeField] private LayerMask lm;
     private Vector3 soma;
     private Ray r;
     void Start()
+<<<<<<< HEAD
+    {    
+=======
     {
-        botaoPrincipal = "A";
+        
+<<<<<<< HEAD
+>>>>>>> 8cf17ae75cf7bceca1b6dc5dbf0a0f9bfad2339e
+=======
+>>>>>>> 46b2a04d5cc61e52d556ea621eedd2c968f9c8df
+        botaoPrincipal = TelaSelecao.teclasEscolhidas[index];
         rbPlayer = GetComponent<Rigidbody2D>();
         _animator = transform.GetChild(0).GetComponent<Animator>();
     }
@@ -26,8 +36,14 @@ public class ShipBehaviour : MonoBehaviour
     private void Movimento()
     {
         soma.y = (anguloRaycast) ? distCentro : -distCentro;
-        colidiu = Physics2D.OverlapCircle(transform.position + soma, raioRC, lm);
+        colididos = Physics2D.OverlapCircleAll(transform.position + soma, raioRC, lm);
+        colidiu = colididos.Length > 1;
+<<<<<<< HEAD
+
         _animator.SetBool("EncontrouParede", colidiu);
+
+=======
+>>>>>>> 46b2a04d5cc61e52d556ea621eedd2c968f9c8df
         rbPlayer.velocity = new Vector3(rbPlayer.velocity.x , anguloDireita ? velocidade : -velocidade);
         if (Input.inputString.ToUpper() == botaoPrincipal && executar && colidiu)
         {
--- Assets/Script/UI/TelaSelecao.cs
+++ Assets/Script/UI/TelaSelecao.cs
@@ -6,25 +6,27 @@ using UnityEngine.SceneManagement;
 public class TelaSelecao : MonoBehaviour
 {
     private GameObject[] players;
-    public static List<string> teclasescolhidas = new List<string>();
     float tempo;
+    public static Dictionary<int, string> teclasEscolhidas = new Dictionary<int, string>();
 
     public void Game(string cena)
     {
         players = GameObject.FindGameObjectsWithTag("Caixa");
         foreach (var i in players)
         {
-            teclasescolhidas.Add(i.GetComponent<CaixaPlayer>().txt_teclaEscolhida.text);
+            var temp = i.GetComponent<CaixaPlayer>();
+            if (temp.ativado)
+                teclasEscolhidas.Add(temp.id, temp.txt_teclaEscolhida.text);
         }
-        StartCoroutine("Esperar");
+        StartCoroutine("Esperar", cena);
 
     }
-        IEnumerator Esperar()
+        IEnumerator Esperar(string cena)
         {
             UI ui = this.GetComponent<UI>();
             tempo = 2;
             ui.Fades(false, tempo, Random.Range(0, 2));
             yield return new WaitForSeconds(tempo);
-        SceneManager.LoadScene("SampleScene");
+            SceneManager.LoadScene(cena);
         }
 }
