b75ef9b929a56240268552c48458f129b95a2c56
giribroba <giribroba@gmail.com>
2019-12-14 01:05:16 -0300

    escolher teclas

---
+++ Assets/Script/UI/TelaSelecao.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TelaSelecao : MonoBehaviour
+{
+    private GameObject[] players;
+    public static List<string> teclasescolhidas = new List<string>();
+    public void Game()
+    {
+        players = GameObject.FindGameObjectsWithTag("Caixa");
+        foreach (var i in players)
+        {
+            teclasescolhidas.Add(i.GetComponent<CaixaPlayer>().txt_teclaEscolhida.text);
+        }
+    }
+}
