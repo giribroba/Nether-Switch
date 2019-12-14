501f61120e7d7ebd607a7b58ee103992c1cb6d56
Gabriel Dias <gabrieldias2032@gmail.com>
2019-12-14 01:26:40 -0300

    vs movimentação basica e raycast

---
+++ Assets/ShipBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehaviour : MonoBehaviour
+{
+    [SerializeField]private float velocidade, raioRC, distCentro;
+    private Rigidbody2D rbPlayer;
+    private bool anguloDireita, executar, colidiu, anguloRaycast;
+    private string botaoPrincipal;
+    [SerializeField] private LayerMask lm;
+    private Vector3 soma;
+    private Ray r;
+
+    void Start()
+    {
+        botaoPrincipal = "A";
+        rbPlayer = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        Movimento();
+    }
+    public void Movimento()
+    {
+        soma.y = (anguloRaycast) ? distCentro : -distCentro;
+        colidiu = Physics2D.OverlapCircle(transform.position + soma, raioRC, lm);
+        rbPlayer.velocity = new Vector3(rbPlayer.velocity.x , anguloDireita ? velocidade : -velocidade);
+        if (Input.inputString.ToUpper() == botaoPrincipal && executar && colidiu)
+        {
+            executar = false;
+            anguloRaycast = !anguloRaycast;
+            anguloDireita = !anguloDireita;
+        }
+        else
+        {
+            executar = true;
+        }
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position + soma, raioRC);
+    }
+}
