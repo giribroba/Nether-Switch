2b214cd714266e0fac2d6081dafafa28892f2ff3
giribroba <giribroba@gmail.com>
2019-12-14 02:09:20 -0300

    telinha barcos

--- Assets/Script/UI/TelaSelecao.cs
+++ Assets/Script/UI/TelaSelecao.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TelaSelecao : MonoBehaviour
 {
     private GameObject[] players;
     public static List<string> teclasescolhidas = new List<string>();
-    public void Game()
+    public void Game(string cena)
     {
         players = GameObject.FindGameObjectsWithTag("Caixa");
         foreach (var i in players)
         {
             teclasescolhidas.Add(i.GetComponent<CaixaPlayer>().txt_teclaEscolhida.text);
         }
+        SceneManager.LoadScene(cena);
     }
 }
