ade4091b4fc8b4ab59434fcc61fbfc77243f000b
Gabriel Dias <gabrieldias2032@gmail.com>
2019-12-14 18:09:19 -0300

    bug play sem player

--- Assets/Script/UI/TelaSelecao.cs
+++ Assets/Script/UI/TelaSelecao.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TelaSelecao : MonoBehaviour
 {
     private static GameObject[] players;
-    float tempo;
+    private float tempo;
     public static Dictionary<int, string> teclasEscolhidas = new Dictionary<int, string>();
 
     public void Game(string cena)
@@ -15,11 +16,13 @@ public class TelaSelecao : MonoBehaviour
         foreach (var i in players)
         {
             var temp = i.GetComponent<CaixaPlayer>();
-            if (temp.ativado)
+            if (temp.ativado && temp.txt_teclaEscolhida.text != "...")
                 teclasEscolhidas.Add(temp.id, temp.txt_teclaEscolhida.text);
         }
-        StartCoroutine("Esperar", cena);
-
+        if (!(teclasEscolhidas.Count == 0))
+        {
+            StartCoroutine("Esperar", cena);
+        }
     }
     public static void ZeraTeclas()
     {
