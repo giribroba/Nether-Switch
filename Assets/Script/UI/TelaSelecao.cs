8abb395db6383df7b67ba61905ffe3fa709fdf8f
cauehcs <caue.hcs@gmail.com>
2019-12-14 08:56:58 -0300

    id e correcao na selecao

--- Assets/Script/UI/TelaSelecao.cs
+++ Assets/Script/UI/TelaSelecao.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 public class TelaSelecao : MonoBehaviour
 {
     private GameObject[] players;
-    public static List<string> teclasescolhidas = new List<string>();
+    public static Dictionary<int, string> teclasEscolhidas = new Dictionary<int, string>();
+
     public void Game(string cena)
     {
         players = GameObject.FindGameObjectsWithTag("Caixa");
         foreach (var i in players)
         {
-            teclasescolhidas.Add(i.GetComponent<CaixaPlayer>().txt_teclaEscolhida.text);
+            var temp = i.GetComponent<CaixaPlayer>();
+            if (temp.ativado)
+                teclasEscolhidas.Add(temp.id, temp.txt_teclaEscolhida.text);
         }
         SceneManager.LoadScene(cena);
     }
