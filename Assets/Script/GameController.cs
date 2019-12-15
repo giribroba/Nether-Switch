a2af80e8cdc26e6b2d9a04deacec145da4b0dadd
giribroba <giribroba@gmail.com>
2019-12-15 12:09:31 -0300

    transicao

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -5,9 +5,12 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private float velocidade;
     Rigidbody2D rbCamera;
+    UI fade;
     [SerializeField] GameObject[] navios;
     private void Awake()
     {
+        fade = transform.GetComponent<UI>();
+        fade.Fades(true,1,Random.Range(0,2));
         TelaSelecao.teclasEscolhidas.Add(10, "A");
         rbCamera = GetComponent<Rigidbody2D>();
         for (int i = 0; i < navios.Length; i++)
